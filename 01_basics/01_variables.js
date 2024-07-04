dd208529bc5ae7ece86665eced3ca9bb45e2b1ce
jitender <164144090+jitender4113@users.noreply.github.com>
2024-07-04 06:47:36 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14435
+let accountemail = "hitesh@google.com"
+var accountpass = "12345" //prefer not to use var only use let
+accountcity = "jaipur"
+let accountstate;
+
+//accountId = 2// not allowed
+
+accountemail = "jatin@gmail.com"
+accountpass = "212211"
+accountcity = "bangalore"
+
+
+console.table([accountId, accountemail, accountpass, accountcity, accountstate])
