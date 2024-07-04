5384137f922f9df17afdc9c4ba0de33785886bdd
jitender <164144090+jitender4113@users.noreply.github.com>
2024-07-04 06:47:53 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3+3)
+
+let name = "hello" //string
+
+// number = 2 to power 55
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone vaalue
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined)
+console.log(typeof null)
