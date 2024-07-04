9d7310582d3294a34313ad5930d9e1414c6d5b4b
jitender <164144090+jitender4113@users.noreply.github.com>
2024-07-04 07:11:32 +0000

    conversion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -6,3 +6,12 @@ console.log(typeof (score));
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
 console.log(valueInNumber) //NaN -> not a number
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(true)
+console.log(+true)
+console.log(+"")
