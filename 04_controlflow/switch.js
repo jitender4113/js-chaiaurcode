46f70b28b15cf10de2e74f145c0b31444609d3b9
jitender <164144090+jitender4113@users.noreply.github.com>
2024-07-13 09:12:55 +0000

    switch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,15 @@
+const month = "march"
+switch (month){
+    case "january":
+        console.log("january")
+        break;
+    case "february":
+        console.log("february")
+        break;
+    case "march":
+        console.log("march")
+        break;
+    default:
+        console.log("default case match")
+        break;
+}
