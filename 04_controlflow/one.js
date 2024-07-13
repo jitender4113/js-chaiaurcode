4b9fabb311c65b25797c5769535b04ccb0d981d3
jitender <164144090+jitender4113@users.noreply.github.com>
2024-07-13 08:38:05 +0000

    ifelse

---
+++ 04_controlflow/one.js
@@ -0,0 +1,10 @@
+const balance = 600
+if (balance > 500) console.log("test")
+
+if(600 === 600){
+    console.log("equal")
+}
+
+if(600 === "600"){ // this === checks the type also
+    console.log("equal")
+}
